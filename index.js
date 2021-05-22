3fa67a36e3482937a3713969b1c56878ae3367c7
AndrewViktorov <aviktorov174@gmail.com>
2021-05-22 19:03:55 +0300

    fix

--- index.js
+++ index.js
@@ -21,28 +21,28 @@ document.addEventListener("keydown", function (event) {
 function makeSound(key) {
     switch (key) {
         case "w":
-            new Audio("sounds/tom-1.mp3").play()
+            new Audio("sounds/tom-1.mp3").play();
             break;
         case "a":
-            new Audio("sounds/tom-2.mp3").play()
+            new Audio("sounds/tom-2.mp3").play();
             break;
         case "s":
-            new Audio("sounds/tom-3.mp3").play()
+            new Audio("sounds/tom-3.mp3").play();
             break;
         case "d":
-            new Audio("sounds/tom-4.mp3").play()
+            new Audio("sounds/tom-4.mp3").play();
             break;
 
         case "j":
-            new Audio("sounds/snare.mp3").play()
+            new Audio("sounds/snare.mp3").play();
             break;
 
         case "k":
-            new Audio("sounds/crash.mp3").play()
+            new Audio("sounds/crash.mp3").play();
             break;
 
         case "l":
-            new Audio("sounds/kick-bass2.mp3").play()
+            new Audio("sounds/kick-bass2.mp3").play();
             break;
 
         default:
